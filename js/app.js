17a90ecf7e8e48b65530a539defa7721d49a0d64
Snita Bharth <snitabharth@hotmail.com>
2016-01-30 11:57:27 +0000

    got the basic game working

--- js/app.js
+++ js/app.js
@@ -10,16 +10,45 @@ $(document).ready(function(){
      console.log("The 'l' key is pressed.");
     }
   }
-});
+  });
 
 
-var cards = ["A", "2", "3", "4", "5"]
+var cards = ["A", "2", "3", "4", "5", "6"]
 
-var ramdomIndex = Math.floor((Math.random()*5));
 
-var currentCard = cards [ramdomIndex]
+//setInterval from line 19 to line 35 to run a loop every 2's === 2000 until a snap occurs, the loop will keep going until someone has won the best of 3
 
-console.log(currentCard)
+ function dealCards() {
+    cards = setInterval()
+  }
+
+  var randomIndex1 = Math.floor((Math.random()*6));
+
+  var currentCard1 = cards[randomIndex1]
+
+  console.log(currentCard1)
+
+
+  var randomIndex2 = Math.floor((Math.random()*6));
+
+  var currentCard2 = cards[randomIndex2]
+
+  console.log(currentCard2)
+
+
+
+var snap = cards[randomIndex1] === cards[randomIndex2]
+
+
+//for(var i=snap; i=cards1[randomIndex1] === cards2[randomIndex2]; i++) {
+  //console.log("snap");
+//}
+
+
+  if (currentCard1 === currentCard2){
+    console.log("snap")
+  }
+
  
 
   
@@ -43,3 +72,12 @@ console.log(currentCard)
 //use console to log everything 
 
 // assign keys a and l to keyup events
+//deal a pack of cards and start a game
+//start turning the cards over 
+//register which player hit key a or l first 
+//write a loop to make the cards to keep turning until two cards match
+//when 2 cards match get a snap
+//winner logic to get winner
+
+
+
