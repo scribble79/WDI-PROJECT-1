8d673a6a2000a7eb6d21100dfae73b77ae99929a
Snita Bharth <snitabharth@hotmail.com>
2016-02-01 16:51:01 +0000

    adding comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//variables for all the functions 
 $(document).ready(function(){
   var cards = ["A", "2", "3", "4", "5", "6"];
   var currentCard1;
@@ -31,7 +32,7 @@ $(document).ready(function(){
     updateScoreBoard();
   });
 
-//allocating keys A and L as two players 
+//allocating keys A and L as two players that allow a snap 
   $(window).on("keyup", checkKeyPressed);
   function checkKeyPressed(e) {
     if(currentCard1 === currentCard2) {
@@ -52,7 +53,7 @@ $(document).ready(function(){
     }
   }
 
-//stop the game at 5 snaps, Mike helped with this function
+//Stop the game at 5 snaps, Mike helped with this function
   function clearTimers() {
     if(snapCount === 5) {
       clearTimeout(timer1);
@@ -60,7 +61,7 @@ $(document).ready(function(){
     }
   }
 
-//randomly select the cards for the game. I managed to get this function waorking and got help to insert the images into the array
+//Randomly select the cards for the game. Allocate images to the card names in the array. Timer to rotate the cards every 2 seconds
   timer1 = setInterval(function(){   
     var randomIndex1 = Math.floor((Math.random()*6));
     currentCard1 = cards[randomIndex1]
@@ -78,88 +79,5 @@ $(document).ready(function(){
   }, 1000);
 });
  
-
-
-
-
-
-// $('textarea').first().keypress(function (e)
-// {
-//     if (e.which === 13) console.log('player1 enter key pressed');
-// }).nextAll().keypress(function (e)
-// {
-//     if (e.which === 13) console.log('player2 enter key pressed');
-// });
-
-
-//setInterval to run a loop every 2's === 2000 until a snap occurs, the loop will keep going until someone has won the best of 3
-
-//win logic to register a winner and to play to 3 and mark the wins up on a scoreboard, and assign the A and L keys to player 1 and player 2  
-    
-
-// var snap = cards[randomIndex1] === cards[randomIndex2]
-
-
-//keys a and l used on the keyboard to allow 2 players to play the game
-
-  // if (currentCard1 === currentCard2)
-  //   {
-  // return snap;
-  // }
-
-
-// function winner(point) {
-//    if (e.keyCode === 65 && currentCard1 === currentCard2)
-//      console.log("win")
-//    else (e.keyCode === 76 && currentCard1 === currentCard2)
-//      console.log("win")
-//  }
-
-
-   //function snapHappened() {
-    //if 
-  //}
-
-  
-  
-
- 
-
-  //eventlisteners that register the a and l key being clicked to register a snap
-  
-
- //function dealCards() {
-   // cards = setInterval(currentCard1 && currentCard2,500)
-  //}
-
- //for(var i=snap; i=cards1[randomIndex1] === cards2[randomIndex2]; i++) {
-   //console.log("snap");
- //}
-
-//Snap game
-//2 player game
-//Snap animation
-//Inputs for player 1 and player 2 to input their names
-//create a textarea tag for players to write their names in, this needs to assign the data input to player 1 and player 2
-
-//Let's play
-//computer deals the pack of cards, animation of cards flicking and sounds
-//computer starts turning oevr the cards from either side revealing 
-
-// keyup events
-//math.random for 5 cards in an array
-// var for last card and current card
-// when key is fired if previous card and current card is = then that player wins
-// var for player 1 and player 2 score
-//use console to log everything 
-
-// assign keys a and l to keyup events
-//deal a pack of cards and start a game
-//start turning the cards over 
-//register which player hit key a or l first 
-//write a loop to make the cards to keep turning until two cards match
-//when 2 cards match get a snap
-//winner logic to get winner
-
 
 
